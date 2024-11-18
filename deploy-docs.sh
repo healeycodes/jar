e5f143f0320c48cab13dda43eda23e53edbf7ebb
Andrew Healey <healeycodes@gmail.com>
2024-11-18 15:10:50 +0000

    nudge ci

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -1,4 +1,5 @@
 python3 framework/cli.py build examples/docs
+
 # project packages must be installed locally so they are bundled correctly when deployed
 cd examples/docs && pip3 install -r requirements.txt --target . && cd ../..
 cd build && vercel --prebuilt --prod && cd ..
